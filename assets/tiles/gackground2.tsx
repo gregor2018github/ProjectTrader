35307cb3c4649f026379e3b4d061a0ac28c9d18a
gregor2018github <krause.gregor1@googlemail.com>
2025-12-22 20:54:42 +0100

    Played around a bit with tiles and. In the next steps these shall be combined with the map demo.

---
+++ assets/tiles/gackground2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="gackground2" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
+ <image source="gackground2.png" width="3200" height="3200"/>
+</tileset>
