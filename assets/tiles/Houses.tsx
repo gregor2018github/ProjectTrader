e48f03de9d2b5f85d4996308df872a0ec1f904c9
gregor2018github <krause.gregor1@googlemail.com>
2026-01-01 22:53:39 +0100

    Work done on displaying houses in the game. Added several assets for houses and brought the first 4 house structures as non-object background tiles into the game. The proportions work fine as long as it is not zoomed out too much, maybe I cut the zoom stages later.

---
+++ assets/tiles/Houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Houses" tilewidth="32" tileheight="32" tilecount="15625" columns="125">
+ <image source="../map_sprites/houses/Houses.png" width="4000" height="4000"/>
+</tileset>
